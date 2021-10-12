d7edb5b641673b3389f5037b6a172654817ff89f
Dmitry Antsibor <d.antsibor@yadro.com>
2021-10-12 15:42:04 +0300

    collect statistic for targetolog campaigns

--- internal/app/service/stat/targetolog.go
+++ internal/app/service/stat/targetolog.go
@@ -1,1 +1,15 @@
 package stat
+
+import (
+	"time"
+
+	"github.com/DmiAS/bd_course/internal/app/models"
+	"github.com/google/uuid"
+)
+
+func (s Service) GetTargetologStat(targetologID uuid.UUID, from, to time.Time) (*models.TargetologStat, error) {
+	rep := s.unit.GetCampaignsRepository()
+	camps := rep.GetCampaigns(targetologID)
+	campsStat := s.getCampsStat(camps, from, to)
+	return &models.TargetologStat{Camps: campsStat}, nil
+}
