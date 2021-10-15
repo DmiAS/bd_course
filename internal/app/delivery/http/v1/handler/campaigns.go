0b7b2f0e809f8a516f243fa18c7e2e6f8b1807f6
Dmitry Antsibor <d.antsibor@yadro.com>
2021-10-15 16:38:27 +0300

    refactor handlers

--- internal/app/delivery/http/v1/handler/campaigns.go
+++ internal/app/delivery/http/v1/handler/campaigns.go
@@ -4,18 +4,25 @@ import (
 	"net/http"
 
 	"github.com/DmiAS/bd_course/internal/app/uwork"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
+type campReq struct {
+	ThreadID     uuid.UUID `json:"thread_id"`
+	TargetologID uuid.UUID `param:"targetolog_id"`
+	CampaignID   uuid.UUID `param:"id"`
+}
+
 // получение кампаний таргетолога
 func (h *Handler) getTargetologCampaigns(ctx echo.Context) error {
-	id, err := extractID(ctx)
+	campID, err := extractID(ctx)
 	if err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
 	}
 
 	ws := h.cmpf.GetService(uwork.Admin)
-	camps := ws.GetCampaigns(id)
+	camps := ws.GetCampaigns(campID)
 	return ctx.JSON(http.StatusOK, camps)
 }
 
@@ -29,12 +36,12 @@ func (h *Handler) getCampaigns(ctx echo.Context) error {
 // привязка кампании к конкретному потоку
 func (h *Handler) attachCampaign(ctx echo.Context) error {
 	req := &campReq{}
-	if err := req.bind(ctx); err != nil {
+	if err := ctx.Bind(req); err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
 	}
 
 	ws := h.cmpf.GetService(uwork.Admin)
-	if err := ws.AttachCampaign(req.ThreadID, req.id); err != nil {
+	if err := ws.AttachCampaign(req.ThreadID, req.CampaignID); err != nil {
 		return ctx.String(http.StatusInternalServerError, err.Error())
 	}
 	return ctx.NoContent(http.StatusOK)
@@ -42,12 +49,12 @@ func (h *Handler) attachCampaign(ctx echo.Context) error {
 
 func (h *Handler) assignCampaign(ctx echo.Context) error {
 	req := &campReq{}
-	if err := req.bind(ctx); err != nil {
+	if err := ctx.Bind(req); err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
 	}
 
 	ws := h.cmpf.GetService(uwork.Admin)
-	if err := ws.AssignCampaign(req.ThreadID, req.id); err != nil {
+	if err := ws.AssignCampaign(req.ThreadID, req.CampaignID); err != nil {
 		return ctx.String(http.StatusInternalServerError, err.Error())
 	}
 	return ctx.NoContent(http.StatusOK)
--- internal/app/delivery/http/v1/handler/tools.go
+++ internal/app/delivery/http/v1/handler/tools.go
@@ -10,56 +10,22 @@ func extractID(ctx echo.Context) (uuid.UUID, error) {
 	return uuid.Parse(sid)
 }
 
-type info struct {
-	Name   string `json:"name"`
-	rootID uuid.UUID
-	id     uuid.UUID
-}
-
-func (i *info) bind(ctx echo.Context) error {
-	rootID := ctx.Param("client_id")
-	if rootID != "" {
-		id, err := uuid.Parse(rootID)
-		if err != nil {
-			return err
-		}
-		i.rootID = id
-	}
-
-	id := ctx.Param("id")
-	if id != "" {
-		id, err := uuid.Parse(id)
-		if err != nil {
-			return err
-		}
-		i.id = id
-	}
-
-	return ctx.Bind(i)
-}
-
-type campReq struct {
-	ThreadID     uuid.UUID `json:"thread_id"`
-	targetologID uuid.UUID
-	id           uuid.UUID
-}
-
-func (c *campReq) bind(ctx echo.Context) error {
-	if id := ctx.Param("targetolog_id"); id != "" {
-		var err error
-		c.targetologID, err = uuid.Parse(id)
-		if err != nil {
-			return err
-		}
-	}
-
-	if id := ctx.Param("id"); id != "" {
-		var err error
-		c.id, err = uuid.Parse(id)
-		if err != nil {
-			return err
-		}
-	}
-
-	return ctx.Bind(c)
-}
+//func (c *campReq) bind(ctx echo.Context) error {
+//	if id := ctx.Param("targetolog_id"); id != "" {
+//		var err error
+//		c.targetologID, err = uuid.Parse(id)
+//		if err != nil {
+//			return err
+//		}
+//	}
+//
+//	if id := ctx.Param("id"); id != "" {
+//		var err error
+//		c.id, err = uuid.Parse(id)
+//		if err != nil {
+//			return err
+//		}
+//	}
+//
+//	return ctx.Bind(c)
+//}
