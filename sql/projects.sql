bf021061a108d68a253b402de6015be8b4c4fbd9
Dmitry Antsibor <d.antsibor@yadro.com>
2021-10-16 22:38:39 +0300

    edit constraint

--- sql/projects.sql
+++ sql/projects.sql
@@ -2,6 +2,7 @@ create table if not exists projects
 (
     id uuid primary key,
     client_id uuid,
-    name varchar(128) not null unique ,
-    foreign key(client_id) references users(id)
+    name varchar(128) not null,
+    foreign key(client_id) references users(id),
+    unique(client_id, name)
 );
