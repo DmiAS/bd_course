eab56200bc0835e2915a722b38799704e8220553
Dmitry Antsibor <d.antsibor@yadro.com>
2021-10-19 23:48:25 +0300

    first threads fixes

--- internal/app/delivery/http/v1/handler/threads.go
+++ internal/app/delivery/http/v1/handler/threads.go
@@ -43,16 +43,15 @@ func (h *Handler) createThread(ctx echo.Context) error {
 		log.Println(err)
 		return ctx.NoContent(http.StatusNonAuthoritativeInfo)
 	}
+
 	data := &threadInfo{}
 	if err := data.bind(ctx); err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
 	}
-
 	ts := h.tf.GetService(info.Role)
 	if err := ts.Create(data.ProjectID, data.Name); err != nil {
 		return ctx.String(http.StatusInternalServerError, err.Error())
 	}
-
 	return ctx.NoContent(http.StatusCreated)
 }
 
@@ -62,11 +61,11 @@ func (h *Handler) getThread(ctx echo.Context) error {
 		log.Println(err)
 		return ctx.NoContent(http.StatusNonAuthoritativeInfo)
 	}
+
 	data := &threadInfo{}
 	if err := data.bind(ctx); err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
 	}
-
 	ts := h.tf.GetService(info.Role)
 	thread, err := ts.Get(data.ThreadID)
 	if err != nil {
@@ -81,6 +80,7 @@ func (h *Handler) getThreadCampaigns(ctx echo.Context) error {
 		log.Println(err)
 		return ctx.NoContent(http.StatusNonAuthoritativeInfo)
 	}
+
 	pag := &models.Pagination{}
 	if err := ctx.Bind(pag); err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
@@ -89,7 +89,6 @@ func (h *Handler) getThreadCampaigns(ctx echo.Context) error {
 	if err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
 	}
-
 	cs := h.cmpf.GetService(info.Role)
 	camps := cs.GetThreadCampaigns(threadID, pag)
 	return ctx.JSON(http.StatusOK, camps)
@@ -101,11 +100,11 @@ func (h *Handler) getProjectThreads(ctx echo.Context) error {
 		log.Println(err)
 		return ctx.NoContent(http.StatusNonAuthoritativeInfo)
 	}
+
 	data := &threadInfo{}
 	if err := data.bind(ctx); err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
 	}
-
 	pag := &models.Pagination{}
 	if err := ctx.Bind(pag); err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
@@ -126,7 +125,6 @@ func (h *Handler) updateThread(ctx echo.Context) error {
 	if err := data.bind(ctx); err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
 	}
-
 	ts := h.tf.GetService(info.Role)
 	if err := ts.Update(&models.Thread{
 		ID:        data.ThreadID,
@@ -145,15 +143,14 @@ func (h *Handler) deleteThread(ctx echo.Context) error {
 		log.Println(err)
 		return ctx.NoContent(http.StatusNonAuthoritativeInfo)
 	}
+
 	data := &threadInfo{}
 	if err := data.bind(ctx); err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
 	}
-
 	ts := h.tf.GetService(info.Role)
 	if err := ts.Delete(data.ThreadID); err != nil {
 		return ctx.String(http.StatusInternalServerError, err.Error())
 	}
-
 	return ctx.NoContent(http.StatusOK)
 }
