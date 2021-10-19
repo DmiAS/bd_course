6032848b3b29da1e5696814a3e1e912da269ea20
Dmitry Antsibor <d.antsibor@yadro.com>
2021-10-19 23:24:06 +0300

    fixing worker accessors

--- internal/app/delivery/http/v1/handler/clients.go
+++ internal/app/delivery/http/v1/handler/clients.go
@@ -19,13 +19,11 @@ func (h *Handler) createClient(ctx echo.Context) error {
 	if err := ctx.Bind(client); err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
 	}
-
 	us := h.uf.GetService(info.Role)
 	auth, err := us.Create(client)
 	if err != nil {
 		return ctx.String(http.StatusInternalServerError, err.Error())
 	}
-
 	return ctx.JSON(http.StatusOK, &models.LogPass{
 		Login:    auth.Login,
 		Password: auth.Password,
@@ -38,26 +36,23 @@ func (h *Handler) updateClient(ctx echo.Context) error {
 		log.Println(err)
 		return ctx.NoContent(http.StatusNonAuthoritativeInfo)
 	}
+	targetID, err := extractID(ctx)
+	if err != nil {
+		return ctx.String(http.StatusBadRequest, "invalid uuid")
+	}
+	if err := canManageAccountData(info.Role, info.ID, targetID, models.AdminRole); err != nil {
+		return ctx.String(http.StatusBadRequest, err.Error())
+	}
 
 	client := &models.User{}
 	if err := ctx.Bind(client); err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
 	}
-
-	targetID, err := extractID(ctx)
-	if err != nil {
-		return ctx.String(http.StatusBadRequest, "invalid uuid")
-	}
-	if info.Role != models.AdminRole && info.ID != targetID {
-		return ctx.String(http.StatusBadRequest, "you can't update other clients accounts")
-	}
-
 	client.ID = targetID
 	us := h.uf.GetService(info.Role)
 	if err := us.Update(client); err != nil {
 		return ctx.String(http.StatusBadRequest, err.Error())
 	}
-
 	return ctx.NoContent(http.StatusOK)
 }
 
@@ -66,9 +61,6 @@ func (h *Handler) getClients(ctx echo.Context) error {
 	if err != nil {
 		log.Println(err)
 		return ctx.NoContent(http.StatusNonAuthoritativeInfo)
-	}
-	if info.Role == models.ClientRole {
-		return ctx.String(http.StatusBadRequest, "you can't watch other clients")
 	}
 
 	pag := &models.Pagination{}
@@ -91,15 +83,11 @@ func (h *Handler) getClient(ctx echo.Context) error {
 	if err != nil {
 		return ctx.String(http.StatusBadRequest, "invalid uuid")
 	}
-	if info.Role == models.ClientRole && info.ID != targetID {
-		return ctx.String(http.StatusBadRequest, "you can't watch other clients")
-	}
 	us := h.uf.GetService(info.Role)
 	admin, err := us.Get(targetID)
 	if err != nil {
 		return ctx.String(http.StatusInternalServerError, err.Error())
 	}
-
 	return ctx.JSON(http.StatusOK, admin)
 }
 
@@ -109,13 +97,9 @@ func (h *Handler) deleteClient(ctx echo.Context) error {
 		log.Println(err)
 		return ctx.NoContent(http.StatusNonAuthoritativeInfo)
 	}
-
 	targetID, err := extractID(ctx)
 	if err != nil {
 		return ctx.String(http.StatusBadRequest, "invalid uuid")
-	}
-	if info.Role != models.AdminRole && info.ID != targetID {
-		return ctx.String(http.StatusBadRequest, "you can't delete clients")
 	}
 
 	as := h.af.GetService(info.Role)
