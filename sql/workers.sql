bb27c63485420a00a3c3ad389fbcf286a77ecb75
Dmitry Antsibor <d.antsibor@yadro.com>
2021-10-20 11:31:51 +0300

    update workers table

--- sql/workers.sql
+++ sql/workers.sql
@@ -2,6 +2,6 @@ create table if not exists workers
 (
     user_id uuid primary key,
     grade    varchar(16),
-    position varchar(32),
+    position varchar(32) default 'targetolog',
     foreign key (user_id) references users(id) on delete cascade
 )
